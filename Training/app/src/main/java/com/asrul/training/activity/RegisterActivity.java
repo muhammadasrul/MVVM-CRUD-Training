b0aaa38f2e2fbaebfa4f0a89bf3a2b709135eb0a
Muhammad Asrul Aji Pangestu <asrulajipangestu@gmail.com>
2020-05-19 10:41:50 +0700

    first-commit

---
+++ Training/app/src/main/java/com/asrul/training/activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.asrul.training.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.asrul.training.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ Training/app/src/main/java/com/asrul/training/rest/ApiConfig.java
@@ -0,0 +1,16 @@
+package com.asrul.training.rest;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiConfig {
+    public static ApiService getApiService() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.43.100/Training/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiService service = retrofit.create(ApiService.class);
+        return service;
+    }
+}
