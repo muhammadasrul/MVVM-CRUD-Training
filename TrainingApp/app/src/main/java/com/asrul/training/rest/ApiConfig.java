5cf32c792f88a37637b4882c7d1d7a0d4cfbe987
Muhammad Asrul Aji Pangestu <asrulajipangestu@gmail.com>
2020-05-19 10:49:31 +0700

    first-commit

--- TrainingApp/app/src/main/java/com/asrul/training/rest/ApiConfig.java
+++ TrainingApp/app/src/main/java/com/asrul/training/rest/ApiConfig.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiConfig {
     public static ApiService getApiService() {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.43.100/Training/")
+                .baseUrl("http://192.168.43.100/TrainingPHP/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
